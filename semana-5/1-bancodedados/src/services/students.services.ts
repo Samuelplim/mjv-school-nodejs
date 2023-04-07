14c881f33aa9ee60d4dfb02ff11e2ee12e2969eb
samuelplim <samuel12nerd@gmail.com>
2023-04-07 15:51:06 -0400

    Feature: database model students

--- semana-5/1-bancodedados/src/services/students.services.ts
+++ semana-5/1-bancodedados/src/services/students.services.ts
@@ -1,67 +1,25 @@
+import studentRepository from "../repositories/student.repository";
 import { Student } from "../models/student.model";
-class StudentsService {
-  students: Array<Student> = [
-    {
-      name: "Samuel Araujo",
-      email: "samuel@gmail.com",
-      document: "30589362062",
-      password: "123456",
-      age: 22,
-      phone: "95991454545",
-    },
-    {
-      name: "Sara Araujo",
-      email: "sara.araujo@gmail.com",
-      document: "30589362062",
-      password: "123456",
-      age: 20,
-    },
-    {
-      name: "Moda Paula",
-      email: "moda@gmail.com",
-      document: "1234567890",
-      password: "123456",
-      age: 24,
-    },
-    {
-      name: "meta facebook",
-      email: "meta@meta.com",
-      document: "1234567890",
-      password: "123456",
-      age: 25,
-    },
-  ];
 
+class StudentsService {
   getAll() {
-    return this.students;
+    return studentRepository.getAll();
   }
 
   getByDocument(document: string) {
-    const student = this.students.find((std) => std.document === document);
-    return student;
+    return studentRepository.getByDocument(document);
   }
 
-  create(student: Student) {
-    this.students.push(student);
+  create(student: typeof Student) {
+    return studentRepository.create(student);
   }
 
   remove(document: string) {
-    const studentIndex = this.students.findIndex(
-      (student) => student.document === document
-    );
-    if (studentIndex === -1) {
-      throw new Error("Estudante não encontrado!");
-    }
-    this.students.splice(studentIndex, 1);
+    return studentRepository.remove(document);
   }
 
-  update(document: string, student: Student) {
-    const studentIndex = this.students.findIndex(
-      (student) => student.document === document
-    );
-    if (studentIndex === -1) throw new Error("Estudante não encontrado!");
-
-    this.students[studentIndex] = student;
+  update(document: string, student: typeof Student) {
+    return studentRepository.update(document, student);
   }
 }
 

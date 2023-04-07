a8a367d8826cb0ae09f221355a3d847dd7dcfc77
samuelplim <samuel12nerd@gmail.com>
2023-04-07 16:09:11 -0400

    Uptaded: Routers Students uptaded

--- semana-5/1-bancodedados/src/routers/students.router.ts
+++ semana-5/1-bancodedados/src/routers/students.router.ts
@@ -3,41 +3,44 @@ import studentsServices from "../services/students.services";
 
 const studentsRouter = Router();
 
-studentsRouter.get("/", (req: Request, res: Response) => {
-  const students = studentsServices.getAll();
+studentsRouter.get("/", async (req: Request, res: Response) => {
+  const students = await studentsServices.getAll();
   res.send(students);
 });
-studentsRouter.get("/:document", (req: Request, res: Response) => {
-  const student = studentsServices.getByDocument(req.params.document);
+studentsRouter.get("/:document", async (req: Request, res: Response) => {
+  const student = await studentsServices.getByDocument(req.params.document);
 
   if (!student) {
     res.status(400).send({ message: "Aluno não encontrado!" });
   }
   res.status(200).send(student);
 });
-studentsRouter.post("/", (req: Request, res: Response) => {
+studentsRouter.post("/", async (req: Request, res: Response) => {
   if (req.body.age < 18) {
     return res.status(400).send({
       message: "Estudante não foi adicionado, pois não tem idade mínima!",
     });
   }
-  studentsServices.create(req.body);
+  await studentsServices.create(req.body);
   res.send({ message: "Estudante adicionado" }).status(201);
 });
 
-studentsRouter.delete("/remove/:document", (req: Request, res: Response) => {
-  try {
-    studentsServices.remove(req.params.document);
-    res.status(200).send({ message: "Estudante removido com sucesso!" });
-  } catch (error: any) {
-    console.log("entrou");
-    res.status(400).send({ message: error.message });
+studentsRouter.delete(
+  "/remove/:document",
+  async (req: Request, res: Response) => {
+    try {
+      await studentsServices.remove(req.params.document);
+      res.status(200).send({ message: "Estudante removido com sucesso!" });
+    } catch (error: any) {
+      console.log("entrou");
+      res.status(400).send({ message: error.message });
+    }
   }
-});
+);
 
-studentsRouter.put("/:document", (req: Request, res: Response) => {
+studentsRouter.put("/:document", async (req: Request, res: Response) => {
   try {
-    studentsServices.update(req.params.document, req.body);
+    await studentsServices.update(req.params.document, req.body);
 
     res.status(200).send({ message: "Estudante atualizado!" });
   } catch (error: any) {
